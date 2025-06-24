b8f2b4fa74df8b53c2e29878fb717946a02d6cc9
sab2000 <sab.2000@ymail.com>
2025-06-24 14:29:07 +0800

    Deploy web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "765700026"
+    serviceWorkerVersion: "2233012644"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "60a338ec88cb07292a1487b319a13b09
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c452ddd61f6e2503e9841ef265e8a054",
+"flutter_bootstrap.js": "410158b4d26597512a2568b20d6e8ec7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
