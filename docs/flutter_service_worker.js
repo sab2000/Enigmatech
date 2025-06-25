f0a3719db64909a4e88f995bae6eea52c17e22d7
sab2000 <sab.2000@ymail.com>
2025-06-25 14:03:17 +0800

    Deploy web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "60a338ec88cb07292a1487b319a13b09
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "10d4f7ce0ce5cc6c75625f5d6a8a53fb",
+"flutter_bootstrap.js": "cab3ebc34c97a7eba1f8b817221a4732",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
